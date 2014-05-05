50191fcff22fb4b5c72bb1190a02ab4d5c9aefe2
Elliott Draper <el@kickcode.com>
2014-05-05 23:18:30 +0100

    custom floating window for note entry

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,13 @@
 class AppDelegate
+  WINDOW_WIDTH = 500
+  WINDOW_HEIGHT = 180
+
   attr_accessor :status_menu
 
   def applicationDidFinishLaunching(notification)
     @app_name = NSBundle.mainBundle.infoDictionary['CFBundleDisplayName']
+
+    self.buildWindow
 
     @status_menu = NSMenu.new
 
@@ -14,6 +19,35 @@ class AppDelegate
     @status_menu.addItem createMenuItem("About #{@app_name}", 'orderFrontStandardAboutPanel:')
     @status_menu.addItem createMenuItem("Add Note", 'pressAddNote')
     @status_menu.addItem createMenuItem("Quit", 'terminate:')
+
+    @window.toggleWithFrame(CGRectMake(0, 0, 0, 0))
+    @window.center
+  end
+
+  def buildWindow
+    @window = Motion::Popup::Panel.alloc.initPopup(WINDOW_WIDTH, WINDOW_HEIGHT)
+    @window.arrow = false
+
+    @rect = NSInsetRect(
+      @window.contentView.frame,
+      @window.background.line_thickness + 5,
+      @window.background.arrow_height)
+
+    @box = NSBox.alloc.initWithFrame(@rect)
+    @box.setTitle("Enter note")
+    @window.contentView.addSubview(@box)
+
+    text_field_frame = NSInsetRect(@rect, 0, 12)
+    text_field_frame.origin.y -= 28
+    @text = NSTextField.alloc.initWithFrame(text_field_frame)
+    @text.stringValue = ""
+    @text.drawsBackground = false
+    @text.setBezeled(false)
+    @text.setBordered(false)
+    @text.cell.setFocusRingType(NSFocusRingTypeNone)
+    @text.setFont(NSFont.fontWithName("Arial", size: 30))
+    @text.setDelegate(self)
+    @box.addSubview(@text)
   end
 
   def createMenuItem(name, action)
